76c4a1e14720ecc394263d72dc29d7fa6062896a
braenn <bleed05@outlook.com>
2024-05-28 13:02:07 +1000

    tubular viewer update 1

--- MatlabScripts/appGcodeT01.m
+++ MatlabScripts/appGcodeT01.m
@@ -3,17 +3,17 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 A = str2double(A); B = str2double(B);
-
-format = '%s %s%.4f %s%.4f %s%.4f\n%s %s%.4f %s%.4f %s%.4f\n'; 
-
 f = Speed*cos(deg2rad(A/2));    % translation speed in X
 rep = Layers*ucQ;               % number of repeated zigzag gcode lines 
-
 x = Length; E = Length*tan(deg2rad(A/2));
 
 fprintf(fileID, '%s%.4f\n','F',f);
-pair = sprintf(format,'G1','X',Length,'E',E,'G1','X-',Length,'E',E);
-%pair = sprintf(format,'G1','X',Length,'E',E,'F',f,'G1','X-',Length,'E',E,'F',f);
+
+format2 = '%s %s%.4f %s%.4f\n%s %s%.4f %s%.4f\n';
+pair = sprintf(format2,'G1','X',Length,'E',E,'G1','X-',Length,'E',E);
+
+%format1 = '%s %s%.4f %s%.4f %s%.4f\n%s %s%.4f %s%.4f %s%.4f\n'; 
+%pair = sprintf(format1,'G1','X',Length,'E',E,'F',f,'G1','X-',Length,'E',E,'F',f);
 
 for n = 1:rep
     fprintf(fileID, '%s',pair);
